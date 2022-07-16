23171d547810df4e20143555dc99c2d41e57f0f6
Jozek Hajduk <hajduksanchez.dev@gmail.com>
2022-07-16 17:03:25 -0500

    🎉 [BEGIN] Start a new project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config() // Gave us the possibility to use the .env file
+
+const app = express() // Create express app
+
+app.listen(process.env.PORT || 3000, () => {
+  const port = process.env.PORT || 3000
+  console.log(`Server running on port ${port}`)
+})
